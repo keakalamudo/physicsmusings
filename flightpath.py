dca9188f686b10e0491498e8568c0625a9809806
keakalamudo <kelo.akalamudo@gmail.com>
2014-06-09 19:40:36 -0700

    gravitation, drag and wind factors done

--- flightpath.py
+++ flightpath.py
@@ -1,116 +1,256 @@
+#Ball Shooting Simulation(Gravity + Air Resistance + Wind)
+
 from visual import *
 from visual.graph import *
 
 ##
 # Set up area on screen to display objects
 ##
-scene = display(title='Spring',
-    x=80, y=10, width=1000, height=300,
+scene = display(title='Throwing a ball',
+     x=100, y=10, width=1200, height=800,  center=(50,50,0),
     background=color.white)
-#
-# set up base to display objects on the scene
-#
-ground = box(length =10, width = 10, height=0.01, pos=(0,-0.25,0),
-             color=color.green)
-#
-# set up graph output
-#
-graph_spring_x = gdisplay(title=' Position x ',xtitle='time(sec)',ytitle='m',
-    x=80, y= 320, width=1000, background=color.white)
-graph_spring_v = gdisplay(title=' Velocity_x  ',xtitle='time(sec)',ytitle='V(m/s)',
-    x=80, y= 720, width=1000, background=color.white)
-#
-# initialize graphical output
-#
-posplot = gcurve(gdisplay=graph_spring_x, color=color.red)
-vplot = gcurve(gdisplay=graph_spring_v, color=color.magenta)
 
-# initialize y Set to 0.5
-y = 2.5
-#
-# set up block object (shows horizontal motion of spring-block system)
-#
-block = box(length =0.5, width = 0.5, height=0.5, pos=(y,0,0), color=color.blue, v=vector(0,0,0))
+##
+# set up graph output
+##
+graph_velocity = gdisplay(title='Velocity',x=1200,y=200,xtitle='time(sec)',ytitle='V(m/s)',
+                   foreground=color.black, background=color.white)
+
+##
+# initialize parameters
+##
+
+dott_radius = 1.5
+x0 = -50
+g = 9.8
+theta = 60*pi/180
+speed = 50
+dt=1
+t=0
+tc = 0
+vix=50*cos(theta)
+viy=50*sin(theta)
+
+
+
+
+##
+# set up base to display objects on the scene
+##
+
+ground = box(pos=(50,-1,0), axis=(250,0,0), lenght=250,height=1,width=20, color=color.green)
+ground.color=color.red
+
+# set processing rate
+myrate=200 #for while loop
+
+# start program
+scene.mouse.getclick()
+
+# print title
+print (('Gravitation only'))
+
+##
+# set up ball object
+##
+
+ball = sphere(pos=( x0 ,0 ,0), mass= 0.2, v=vector(speed*cos(theta), speed*sin(theta),0),radius=0.5, color=color.blue,  make_trail=True  )
+ball.p = ball.mass*ball.v
+ballvy_old = 0
+
+##
+# initialize graphical output
+##
+
+vx_curve    = gcurve(gdisplay=graph_velocity, color=ball.color)
+vy_curve    = gcurve(gdisplay=graph_velocity, color=ball.color)
+
+##
+# Place initial ball
+##
+
+sphere(pos=ball.pos, radius=dott_radius, color=ball.color)
+
+
+while(ball.y>=0):
+    rate(myrate)
+
+    # define force
+    FG = vector (0,-ball.mass*g, 0)
+    # update momentum
+    ball.p += FG*dt
+    # update velocity
+    ball.v = (ball.p/ball.mass)
+    # update position
+    ball.pos += (ball.p/ball.mass)*dt
+
+
+    # velocity for plot x ,y
+    vx = ball.p.x/ball.mass
+    vy = ball.p.y/ball.mass
+
+
+
+    vx_curve.plot(pos=(t,vx))
+    vy_curve.plot(pos=(t,vy))
+
+
+    if ballvy_old*ball.v.y <0 :
+        print("Max height is "+ str(ball.pos.y) +"m")
+    ballvy_old = ball.v.y
+    t+=dt
+    tc+=dt
+    if tc>1.0:
+        tc =0
+        sphere(pos=ball.pos, radius=dott_radius, color =ball.color)
+print(" time= %.1f s , distance = %.2f m "%(t,ball.pos.x-x0))
+print("")
+#########################################################################################################
+print (('Gravitation + Air drag:'))
+
 #
 # initialize parameters
 #
-m = 0.04
-k = 1.4
-l = 2
-dl =0.5
+
+dott_radius = 1.5
+x0 = -50
 g = 9.8
-cc =30
-rho = 0.005
-block.p = m*block.v
-#
-# initialize time parameters
-#
+theta = 60*pi/180
+speed = 50
 dt=.01
 t=0
 tc = 0
 
+
+cc =0.5
+rho = 0.005
+
+ball = sphere(pos=( x0 ,0 ,0), mass= 0.2, v=vector(speed*cos(theta), speed*sin(theta),0) ,
+              radius=0.5, color=color.green,  make_trail=True  )
+
+ball.p = ball.mass*ball.v
+
+ballvy_old = ball.v.y
+
+vx_curve    = gcurve(gdisplay=graph_velocity, color=ball.color)
+vy_curve    = gcurve(gdisplay=graph_velocity, color=ball.color)
+
 #
-# start program with a mouse click
+# Place initial ball
 #
-scene.mouse.getclick()
-# set processing rate
-myrate = 1000
-# print title
-print (" Spring force + quadratic air drag ")
-w = sqrt(k/m)
-period = (2*pi)/w
-bx_old = block.pos.x
-t1=0
-##############################################
-# process loop (action)   ####################
-##############################################
-while (t < 8):
+
+sphere(pos=ball.pos, radius=dott_radius, color=ball.color)
+
+
+while(ball.y>=0):
     rate(myrate)
-    ##############################################
-    # PHYSICS    >>>>>>             ##############
-    ##############################################
-    # calculate spring force
-    y = block.pos.x
-    FS = vector(-k*(y-l),0,0)
-    #print(FS)
-    # if damped oscillator - calculate air resistance
-    fair = -0.5*cc*rho*(block.width**2) * (mag(block.v)**2) * norm(block.v)
-    # calculate total force on block
-    FT=FS+fair
-    # MOMENTUM PRINCIPLE
-    block.p += FT*dt
-    # update block velocity
-    block.v = (block.p/m)
-    # update block position
-    block.pos +=(block.p/m)*dt
-    y = block.pos.x
-    ##############################################
-    # PHYSICS    <<<<<<<<<    ####################
-    ##############################################
-    # update time paramters
+
+    # define force
+    FG = vector (0,-ball.mass*g, 0)
+    fair = -0.5*cc*rho*pi*(ball.radius**2) * (mag(ball.v)**2) * norm(ball.v)
+    f =FG+fair
+    # update momentum
+    ball.p += f*dt
+    # update velocity
+    ball.v = (ball.p/ball.mass)
+    # update position
+    ball.pos += (ball.p/ball.mass)*dt
+
+
+    # velocity for plot x ,y
+    vx = ball.p.x/ball.mass
+    vy = ball.p.y/ball.mass
+
+
+
+    vx_curve.plot(pos=(t,vx))
+    vy_curve.plot(pos=(t,vy))
+
+    if ballvy_old*ball.v.y <=0 :
+        print("Max height is "+ str(ball.pos.y) +"m")
+    ballvy_old = ball.v.y
+
     t+=dt
     tc+=dt
-    # plot position and velocity
-    pos = block.p.x/m
-    velo = block.v.x/m
+    if tc>1.0:
+        tc =0
+        sphere(pos=ball.pos, radius=dott_radius, color =ball.color)
+print(" time= %.1f s , distance = %.2f m "%(t,ball.pos.x-x0))
+print("")
 
-    posplot.plot(pos=(t,pos))
-    vplot.plot(pos=(t,velo))
+############################################################################
+print (('Gravitation + Air drag + Wind'))
+
+#
+# initialize parameters
+#
+
+dott_radius = 1.5
+x0 = -50
+g = 9.8
+theta = 60*pi/180
+speed = 50
+dt=.01
+t=0
+tc = 0
+
+cc =0.5
+rho = 0.005
 
 
-    # Find Period and Calculate period; print them out
-    # Try and find your own method, I will help you out if you
-    # are struggling.
-    #print(block.pos)
-    if ((bx_old < 2) and (block.pos.x > 2)) :
-        print(" t = %.4f " %(t))
 
-        if t1 > 0 :
-                T = (t - t1)
-                print (" T_comp = %.3f     T_exact = %.3f   " %(T,period ))
-    # update time paramters
-        t1 = t
-    # update position paramter to find period
-    bx_old = block.pos.x
+ball = sphere(pos=( x0 ,0 ,0), mass= 0.2, v=vector(speed*cos(theta), speed*sin(theta),0) ,
+              radius=0.5, color=color.yellow,  make_trail=True  )
 
-######### END OF PROGRAM #######################################
+ball.p = ball.mass*ball.v
+
+ballvy_old = ball.v.y
+#
+
+vx_curve    = gcurve(gdisplay=graph_velocity, color=ball.color)
+vy_curve    = gcurve(gdisplay=graph_velocity, color=ball.color)
+
+#
+# Place initial ball
+#
+
+sphere(pos=ball.pos, radius=dott_radius, color=ball.color)
+
+
+while(ball.y>=0):
+    rate(myrate)
+
+    # define force
+    FG = vector (0,-ball.mass*g, 0)
+    wind=vector(-20,0,0)
+    fair = -0.5*cc*rho*pi*(ball.radius**2) * (mag(ball.v-wind)**2) * norm(ball.v-wind)
+    f =FG+fair
+    # update momentum
+    ball.p += f*dt
+    # update velocity
+    ball.v = (ball.p/ball.mass)
+    # update position
+    ball.pos += (ball.p/ball.mass)*dt
+
+
+    # velocity for plot x ,y
+    vx = ball.p.x/ball.mass
+    vy = ball.p.y/ball.mass
+
+
+
+    vx_curve.plot(pos=(t,vx))
+    vy_curve.plot(pos=(t,vy))
+
+
+    if ballvy_old*ball.v.y <=0 :
+        print("Max height is "+ str(ball.pos.y) +"m")
+    ballvy_old = ball.v.y
+
+    t+=dt
+    tc+=dt
+    if tc>1.0:
+        tc =0
+        sphere(pos=ball.pos, radius=dott_radius, color =ball.color)
+print(" time= %.1f s , distance = %.2f m "%(t,ball.pos.x-x0))
+print("")
