5681ae9461cf2b1b9dec9819922b1807db431319
keakalamudo <kelo.akalamudo@gmail.com>
2014-06-26 13:17:21 -0700

    graph plots

--- earthsunsystem.py
+++ earthsunsystem.py
@@ -1,4 +1,4 @@
-# write code for libraries need by Vpython
+﻿# write code for libraries need by Vpython
 from __future__ import division
 from visual import *
 from visual.graph import*
@@ -10,8 +10,9 @@ scene = display(title='Sun - Earth System',x=100, y=10, width=1000, height=1000,
 
 #
 # set up earth and sun objects
-Sun = sphere(pos=( 0 ,0 ,0), mass= 2e30, radius=7e8*10, color=color.orange velocity = vector(0,0,0))
-Earth = sphere(pos=( r0_es ,0 ,0), mass= 6e24, radius=6.4e6*500, color=color.blue,  make_trail=True velocity = vector(2e4,0,0))
+r0_es= 1.496e11
+Sun = sphere(pos=( 0 ,0 ,0), mass= 2e30, radius=7e8*10, color=color.orange, velocity= vector(0,0,0))
+Earth = sphere(pos=( r0_es ,0 ,0), mass= 6e24, radius=6.4e6*500, color=color.blue,  make_trail=True, velocity = vector(2e4,0,0))
 
 
 # define the path of the Earth
@@ -38,7 +39,6 @@ kpe_curve =gcurve(gdisplay=graph_energy, color=color.red)
 # set up graph output for angular momentum plots
 #
 
-
 #
 # initialize graphical output for angular momentum plots
 #
@@ -46,7 +46,8 @@ kpe_curve =gcurve(gdisplay=graph_energy, color=color.red)
 #
 # set up graph output for velocity plots
 #
-
+graph_velocity = gdisplay(title='Velocity ( v_x - red, v_y - blue, v_z – green)' ,x=1200,y=200,xtitle='time(sec)',ytitle='V(m/s)',
+foreground=color.black, background=color.white)
 
 #
 # initialize graphical output for velocity plots
@@ -55,40 +56,44 @@ kpe_curve =gcurve(gdisplay=graph_energy, color=color.red)
 #
 # initialize time parameter
 #
- # one day
+dt =3600 # one day
+t = 0
 
 # start program with mouse click
 
 ##############################################
 # process loop (action)   ####################
 ##############################################
-
+while t<(2*366*10*24*60*60):
                   # plot for two years
                    # shows 100 points in a second
- 
+    rate(2400)
     ##############################################
     # PHYSICS    >>>>>>             ##############
     ##############################################
     # calculate force 
-                                                # Sun - Earth vector
-                                                # sqrt(r_vector.x**2 + r_vector.y**2 + r_vector.z**2 ) # distance
-                                                # t vector
-                                                # Newton's gravitational force
-                                                # MOMENTUM PRINCIPLE
-                                                # update momentum
-     # update position
-                      # draw a line
-                # update velocity
-    # calculate energy 
-                                              # kinetic energy
-                                              # potential energy
+    r_vector = Earth.pos - Sun.pos           # Sun - Earth vector
+    r =mag(r_vector)                         # sqrt(r_vector.x**2 + r_vector.y**2 + r_vector.z**2 ) # distance
+    rhat = r_vector/r                       # t vector
+    F= - G * Earth.mass*Sun.mass/r**2*rhat                             # Newton's gravitational force
+    Earth.p += F*dt                                                # MOMENTUM PRINCIPLE
+    Earth.pos +=  (Earth.p/Earth.mass)*dt  #update position        
+    path.append(pos=Earth.pos) #draw a line
+    ke= (mag(Earth.p))**2/(2*Earth.mass) #kinetic energy
+    pe= -G*Earth.mass*Sun.mass/r #potential energy
+    
+    t+= dt #update time
+
     # calculate angular momentum
 
     ##############################################
     # PHYSICS    <<<<<<<<<    ####################
     ##############################################
     # plot energy
-
+    kpe= ke+ pe #kinetic + potential energy
+    ke_curve.plot(pos=(t,ke))
+    pe_curve.plot(pos=(t,pe))
+    pe_curve.plot(pos=(t,kpe))
     # plot velocity
     
     # plot angular momentum
