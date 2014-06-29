6d1ca35c497e1c7c14c075f6e1cfc4c7b4f84b8c
keakalamudo <kelo.akalamudo@gmail.com>
2014-06-28 18:37:59 -0700

    complete work

--- earthsunsystem.py
+++ earthsunsystem.py
@@ -11,8 +11,8 @@ scene = display(title='Sun - Earth System',x=100, y=10, width=1000, height=1000,
 #
 # set up earth and sun objects
 r0_es= 1.496e11
-Sun = sphere(pos=( 0 ,0 ,0), mass= 2e30, radius=7e8*10, color=color.orange, velocity= vector(0,0,0))
-Earth = sphere(pos=( r0_es ,0 ,0), mass= 6e24, radius=6.4e6*500, color=color.blue,  make_trail=True, velocity = vector(2e4,0,0))
+Sun = sphere(pos=( 0 ,0 ,0), mass= 2e30, radius=7e8*10, color=color.orange, v= vector(0,0,0))
+Earth = sphere(pos=(r0_es ,0 ,0), mass= 6e24, radius=6.4e6*500, color=color.blue,  make_trail=True, v = vector(2e4,0,0))
 
 
 # define the path of the Earth
@@ -22,7 +22,7 @@ path.append(pos=Earth.pos)
 # initialize earth and sun paramters 
 #
 G = 6.67384e-11
-Earth.p = Earth.mass*Earth.velocity
+Earth.p = Earth.mass*Earth.v
 #
 # set up graph output for energy plots
 #
@@ -58,8 +58,10 @@ foreground=color.black, background=color.white)
 #
 dt =3600 # one day
 t = 0
+tc = 0
 
 # start program with mouse click
+dott_radius = 4
 
 ##############################################
 # process loop (action)   ####################
@@ -73,33 +75,37 @@ while t<(2*366*10*24*60*60):
     ##############################################
     # calculate force 
     r_vector = Earth.pos - Sun.pos           # Sun - Earth vector
-    r =mag(r_vector)                         # sqrt(r_vector.x**2 + r_vector.y**2 + r_vector.z**2 ) # distance
-    rhat = r_vector/r                       # t vector
-    F= - G * Earth.mass*Sun.mass/r**2*rhat                             # Newton's gravitational force
-    Earth.p += F*dt                                                # MOMENTUM PRINCIPLE
-    Earth.pos +=  (Earth.p/Earth.mass)*dt  #update position        
-    path.append(pos=Earth.pos) #draw a line
-    ke= (mag(Earth.p))**2/(2*Earth.mass) #kinetic energy
-    pe= -G*Earth.mass*Sun.mass/r #potential energy
-    
-    t+= dt #update time
+    centForce = (-(6.67e-11)* Earth.mass*(Sun.mass)/r0_es**2)
+    FC = centForce*norm(r_vector)
 
+     # MOMENTUM PRINCIPLE
+    Earth.p += FC*dt
+    # update ball velocity
+    Earth.v = (Earth.p/Earth.mass)
+    # update ball position
+    Earth.pos += (Earth.v)*dt
     # calculate angular momentum
 
     ##############################################
     # PHYSICS    <<<<<<<<<    ####################
     ##############################################
     # plot energy
-    kpe= ke+ pe #kinetic + potential energy
-    ke_curve.plot(pos=(t,ke))
-    pe_curve.plot(pos=(t,pe))
-    pe_curve.plot(pos=(t,kpe))
+    #kpe= ke+ pe #kinetic + potential energy
+    #ke_curve.plot(pos=(t,ke))
+    #pe_curve.plot(pos=(t,pe))
+    #pe_curve.plot(pos=(t,kpe))
     # plot velocity
     
     # plot angular momentum
  
     # update time
-             
+    t+=dt
+    tc+=dt            
 
     
-    
+    if tc>1.0:
+        tc =0
+        sphere(pos=Earth.pos, radius=dott_radius, color =color.red)
+# print final position
+#print(" time= %.1f s , distance = %.2f m "%(t,ball.pos.x-x0))
+print ("  "  )
